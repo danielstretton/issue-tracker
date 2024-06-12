59ae9d34acc1e06abf48982576a105a6a5a8061b
Daniel Stretton <danielstretton1983@gmail.com>
2024-06-12 22:44:56 +0100

    nav-bar styling

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { AiFillBug } from "react-icons/ai";
 import { useSession } from "next-auth/react";
-import { Box } from "@radix-ui/themes";
+import { Box, Container, Flex } from "@radix-ui/themes";
 
 const NavBar = () => {
     const path = usePathname();
@@ -24,36 +24,43 @@ const NavBar = () => {
     ];
 
     return (
-        <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center text-zinc-900">
-            <Link href="/">
-                <AiFillBug />
-            </Link>
-            <ul className="flex space-x-6">
-                {links.map((link) => {
-                    return (
-                        <li key={link.href}>
-                            <Link
-                                className={classNames({
-                                    "text-zinc-500": link.href !== path,
-                                    "hover:text-zinc-800 transition-colors":
-                                        true,
-                                })}
-                                href={link.href}
-                            >
-                                {link.label}
-                            </Link>
-                        </li>
-                    );
-                })}
-            </ul>
-            <Box>
-                {status === "authenticated" && (
-                    <Link href="/api/auth/signout">Sign out</Link>
-                )}
-                {status === "unauthenticated" && (
-                    <Link href="/api/auth/signin">Sign in</Link>
-                )}
-            </Box>
+        <nav className="border-b mb-5 px-5 py-3 text-zinc-900">
+            <Container>
+                <Flex justify="between">
+                    <Flex align="center" gap="3">
+                        <Link href="/">
+                            <AiFillBug />
+                        </Link>
+                        <ul className="flex space-x-6">
+                            {links.map((link) => {
+                                return (
+                                    <li key={link.href}>
+                                        <Link
+                                            className={classNames({
+                                                "text-zinc-500":
+                                                    link.href !== path,
+                                                "hover:text-zinc-800 transition-colors":
+                                                    true,
+                                            })}
+                                            href={link.href}
+                                        >
+                                            {link.label}
+                                        </Link>
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </Flex>
+                    <Box>
+                        {status === "authenticated" && (
+                            <Link href="/api/auth/signout">Sign out</Link>
+                        )}
+                        {status === "unauthenticated" && (
+                            <Link href="/api/auth/signin">Sign in</Link>
+                        )}
+                    </Box>
+                </Flex>
+            </Container>
         </nav>
     );
 };
